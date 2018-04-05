6a897aa7eccb6d3aaa524dd2dc2b0b54664235ad
Unknown <ajbeers89@gmail.com>
2018-04-05 12:39:02 -0500

    drag drop

---
+++ WebApplication2/Calls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Calls
+{
+	public Calls()
+	{
+	}
+
+    public bool CheckEmpID(string userId, string userPassword)
+    {
+        // call DB check for userid and password
+        // get responce
+        // pass responce
+        return true;
+    }
+    public void LogIn(string userId, string userPassword)
+    { 
+        // check id authentication
+        bool answer = CheckEmpID(userId, userPassword);
+        
+    }
+    public string GetRole(string empID)
+    {
+        string role;
+        string workID;
+        //call DB ask for rolls of empID
+        return role;
+    }
+    public void newPunch(string empID, int workID, DateTime punchTime)
+    {
+        if(checkClockedIn = false)
+        {
+            // create and log new punch
+        }
+        else
+        {
+            // error
+        }
+    }
+    public bool checkClockedIn(string empID)
+    {
+        //check DB for last log with empID
+        //check log for null data in clock out
+
+        // if null -- still clocked in
+        return true;
+        // if not null -- clocked out
+        return false;
+    }
+
+    }
+}
